2c29f7d2cf89cd0a1dac8241bdb5542f875bc518
Zachary Zajdel <zaczajdel213@gmail.com>
2024-07-21 15:18:25 -0500

    feat: setup t3 env and zod

---
+++ env.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import { createEnv } from "@t3-oss/env-nextjs";
+ 
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+    AUTH_SECRET: z.string().min(1),
+    AUTH_GOOGLE_ID: z.string().min(1),
+    AUTH_GOOGLE_SECRET: z.string().min(1),
+  },
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
+    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
+  },
+});
