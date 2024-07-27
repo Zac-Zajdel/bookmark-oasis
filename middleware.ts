c436c2a53f84ded39b7c053d0a6021f3fd08c5e8
Zachary Zajdel <zaczajdel213@gmail.com>
2024-07-27 02:41:05 -0500

    fix: adjust middleware

--- middleware.ts
+++ middleware.ts
@@ -3,19 +3,17 @@ import { env } from '@/env';
 import { NextResponse } from 'next/server';
 
 export default auth((req) => {
-  if (req.auth && req.url.includes('/login')) {
+  const isAuthRoute = /\/(login|signup)/.test(req.url);
+
+  if (req.auth && isAuthRoute) {
     return NextResponse.redirect(new URL('/dashboard', req.url));
   }
 
-  if (
-    !req.auth &&
-    req.url !== env.NEXT_PUBLIC_APP_URL &&
-    !req.url.includes('/login')
-  ) {
+  if (!req.auth && req.url !== env.NEXT_PUBLIC_APP_URL && !isAuthRoute) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 });
 
 export const config = {
-  matcher: ['/dashboard', '/login'],
+  matcher: ['/dashboard', '/login', '/signup'],
 };
