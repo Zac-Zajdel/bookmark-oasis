b2bbf656fc1495e16e5aeb30f9bd173d016398a0
Zachary Zajdel <zaczajdel213@gmail.com>
2024-08-04 01:28:55 -0500

    feat: external api route capabilities

---
+++ app/api/external-tokens/route.ts
@@ -0,0 +1,35 @@
+import { withAuthManager } from '@/lib/authManager';
+import { prisma } from '@/lib/db';
+import { hashApiToken } from '@/lib/utils';
+import { createApiTokenSchema } from '@/lib/zod/apiTokens';
+import { randomBytes } from 'crypto';
+import { NextResponse } from 'next/server';
+
+export const POST = withAuthManager(async ({ req, user }) => {
+  const schema = createApiTokenSchema();
+  const { name } = schema.parse(await req.json());
+
+  // Generate a 32-byte token for user to include in API requests.
+  const apiToken = randomBytes(32).toString('hex');
+
+  // Generate SHA-256 token for database storage.
+  const hashedApiToken = hashApiToken(apiToken);
+
+  await prisma.apiToken.create({
+    data: {
+      userId: user.id,
+      name: name,
+      token: hashedApiToken,
+    },
+  });
+
+  return NextResponse.json(
+    {
+      success: true,
+      message:
+        "Store this API Token in a secure place. You won't be able to retrieve it again later.",
+      data: apiToken,
+    },
+    { status: 201 },
+  );
+});
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from 'clsx';
+import { createHash } from 'crypto';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function hashApiToken(apiToken: string): string {
+  return createHash('sha256').update(apiToken).digest('hex');
 }
