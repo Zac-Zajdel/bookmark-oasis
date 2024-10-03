73112227e55b38d5ac9a5008a0392c68b26bb16e
Zachary Zajdel <zaczajdel213@gmail.com>
2024-10-02 22:00:38 -0500

    feat: adjust seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -26,7 +26,7 @@ async function seedBookmarks(user: User): Promise<void> {
         title: 'Youtube',
         description:
           'Enjoy the videos and music you love, upload original content, and share it all with friends, family, and the world on YouTube.',
-        imageUrl: 'https://www.youtube.com/img/desktop/yt_1200.png',
+        imageUrl: 'https://www.youtube.com/s/desktop/87338098/img/favicon.ico',
       },
       {
         userId: user.id,
@@ -34,17 +34,7 @@ async function seedBookmarks(user: User): Promise<void> {
         title: 'Github',
         description:
           'GitHub is where over 100 million developers shape the future of software, together. Contribute to the open source community, manage your Git repositories, review code like a pro, track bugs and fea...',
-        imageUrl:
-          'https://github.githubassets.com/assets/campaign-social-031d6161fa10.png',
-      },
-      {
-        userId: user.id,
-        url: 'https://tailwindcss.com/',
-        title: 'Tailwind CSS',
-        description:
-          'Tailwind CSS is a utility-first CSS framework for rapidly building modern websites without ever leaving your HTML.',
-        imageUrl:
-          'https://cdn.prod.website-files.com/63bd42e1e586a852754fab62/63e397fec7605a21a97bb284_Open-Graph-1.png',
+        imageUrl: 'https://github.githubassets.com/favicons/favicon.svg',
       },
       {
         userId: user.id,
@@ -53,7 +43,7 @@ async function seedBookmarks(user: User): Promise<void> {
         description:
           "Vercel's Frontend Cloud gives developers the frameworks, workflows, and infrastructure to build a faster, more personalized web.",
         imageUrl:
-          'https://cdn.prod.website-files.com/63bd42e1e586a852754fab62/63e397fec7605a21a97bb284_Open-Graph-1.png',
+          'https://assets.vercel.com/image/upload/front/favicon/vercel/57x57.png',
       },
       {
         userId: user.id,
@@ -62,7 +52,7 @@ async function seedBookmarks(user: User): Promise<void> {
         description:
           'Unleash your creativity with out-of-this-world web design inspiration from across the web.',
         imageUrl:
-          'https://cdn.prod.website-files.com/63bd42e1e586a852754fab62/63e397fec7605a21a97bb284_Open-Graph-1.png',
+          'https://cdn.prod.website-files.com/63bd42e1e586a852754fab62/663a8a90b2b88429f565c65c_curated-logo-favicon.png',
       },
     ],
   });
