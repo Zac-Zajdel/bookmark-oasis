355fbaa6e0c07b84b74d2312cabb40bc9d811f42
Zachary Zajdel <zaczajdel213@gmail.com>
2024-10-21 23:25:08 -0500

    feat: add folders table to schema

---
+++ prisma/migrations/20241022042326_create_folders_table/migration.sql
@@ -0,0 +1,27 @@
+-- AlterTable
+ALTER TABLE "bookmarks" ADD COLUMN     "folder_id" TEXT;
+
+-- CreateTable
+CREATE TABLE "folders" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "parent_id" TEXT,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "icon_name" TEXT,
+    "visits" INTEGER NOT NULL DEFAULT 0,
+    "is_favorite" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "folders_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "bookmarks" ADD CONSTRAINT "bookmarks_folder_id_fkey" FOREIGN KEY ("folder_id") REFERENCES "folders"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "folders" ADD CONSTRAINT "folders_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "folders" ADD CONSTRAINT "folders_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "folders"("id") ON DELETE SET NULL ON UPDATE CASCADE;
