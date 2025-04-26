d438423d7ead1f85ca3bc4976a1ddd04dde1fb0e
Zachary Zajdel <zaczajdel213@gmail.com>
2025-04-25 20:46:05 -0500

    feat: implement multiple-selector component for tag association

--- app/settings/tags/page.tsx
+++ app/settings/tags/page.tsx
@@ -5,6 +5,7 @@ import { DataTableToolbar } from '@/components/tables/data-table-toolbar';
 import { TagActionDialog } from '@/components/tags/tag-action-dialog';
 import { tagTableColumns } from '@/components/tags/tags-table-columns';
 import { Button } from '@/components/ui/button';
+import MultipleSelector, { Option } from '@/components/ui/multiple-selector';
 import { SectionHeader } from '@/components/ui/section-header';
 import { useTagsQuery } from '@/hooks/api/tags/useTagsQuery';
 import { useDataTable } from '@/hooks/useDataTable';
@@ -12,6 +13,20 @@ import { useTableSortingParams } from '@/hooks/useTableSortingParams';
 import { Tag } from '@prisma/client';
 import { TagIcon } from 'lucide-react';
 import { useEffect, useState } from 'react';
+
+const OPTIONS: Option[] = [
+  { label: 'nextjs', value: 'nextjs' },
+  { label: 'React', value: 'react' },
+  { label: 'Remix', value: 'remix' },
+  { label: 'Vite', value: 'vite' },
+  { label: 'Nuxt', value: 'nuxt' },
+  { label: 'Vue', value: 'vue' },
+  { label: 'Svelte', value: 'svelte' },
+  { label: 'Angular', value: 'angular' },
+  { label: 'Ember', value: 'ember', disable: true },
+  { label: 'Gatsby', value: 'gatsby', disable: true },
+  { label: 'Astro', value: 'astro' },
+];
 
 export default function TagsSettings() {
   const [total, setTotal] = useState(0);
@@ -40,6 +55,15 @@ export default function TagsSettings() {
     }
   }, [data]);
 
+  const mockSearch = async (value: string): Promise<Option[]> => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const res = OPTIONS.filter((option) => option.value.includes(value));
+        resolve(res);
+      }, 1000);
+    });
+  };
+
   return (
     <div>
       <SectionHeader
@@ -56,6 +80,30 @@ export default function TagsSettings() {
           }
         />
       </SectionHeader>
+
+      <div className="w-[56rem] py-10">
+        <MultipleSelector
+          defaultOptions={OPTIONS}
+          onSearch={async (value) => {
+            const res = await mockSearch(value);
+            return res;
+          }}
+          hidePlaceholderWhenSelected
+          placeholder="Search for tags..."
+          creatable
+          hideClearAllButton
+          loadingIndicator={
+            <p className="text-muted-foreground py-2 text-center text-lg leading-10">
+              loading...
+            </p>
+          }
+          emptyIndicator={
+            <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
+              no results found.
+            </p>
+          }
+        />
+      </div>
 
       <div className="pb-3">
         <DataTableToolbar
